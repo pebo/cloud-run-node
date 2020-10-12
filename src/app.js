81646991291c1216eec99b769f75a4da0ecebd7a
Per Bockman <per.bockman@gmail.com>
2020-10-12 08:45:00 +0200

    Catch SIGINT and SIGTERM

--- src/app.js
+++ src/app.js
@@ -11,16 +11,38 @@ app.get('/', (req, res) => {
 });
 
 app.get('/gcs', async (req, res) => {
-    console.log('GCS test');
-    const path = 'LC08/01/001/004/LC08_L1GT_001004_20150730_20170406_01_T2/LC08_L1GT_001004_20150730_20170406_01_T2_MTL.txt';
+  console.log('GCS test');
+  const path = 'LC08/01/001/004/LC08_L1GT_001004_20150730_20170406_01_T2/LC08_L1GT_001004_20150730_20170406_01_T2_MTL.txt';
 
-    const file = storage.bucket('gcp-public-data-landsat').file(path);
-    const data = await file.download();
-  
-    res.send(data);
+  const file = storage.bucket('gcp-public-data-landsat').file(path);
+  const data = await file.download();
+
+  res.send(data);
 });
 
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Hello world listening on port', port);
+});
+
+
+// https://blog.heroku.com/best-practices-nodejs-errors
+function shutdown(message) {
+  console.log(message);
+  server.close(() => {
+    process.exit(0)
+  });
+  // If server hasn't finished in 1000ms, shut down process
+  setTimeout(() => {
+    process.exit(0)
+  }, 1000).unref() // Prevents the timeout from registering on event loop
+}
+
+process.on('SIGTERM', () => {
+  shutdown('SIGTERM signal');
+});
+
+process.on('SIGINT', () => {
+  shutdown('SIGINT signal');
 });
